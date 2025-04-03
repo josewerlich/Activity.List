bf3f437a47db279a84de310c9b86d37f75cb30f5
josewerlich <werlich@outlook.com>
2025-04-03 18:40:07 -0400

    Activity02 complete

---
+++ ActivityList.Activity02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ActivityList.Activity02
+{
+    internal class Program
+    {
+        //create a program to calculate a cilinder 
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("Type the radius of the cilinder:");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Type the height of the cilinder:");
+            double height = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region processing 
+            double result = Math.PI * Math.Pow(radius, 2) * height;
+            #endregion
+
+            #region output
+            Console.WriteLine($"The volume of the cilinder is: {result}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
