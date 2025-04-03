ca589f958c565348020d55f8a640db9b41e05fd6
josewerlich <werlich@outlook.com>
2025-04-03 19:05:07 -0400

    Activity 04 - Convert from Celsius to Fahrenheit

--- ActivityList.Activity04/Program.cs
+++ ActivityList.Activity04/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        //create a program to conver Celsius to Fahrenheit
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region input
+            Console.WriteLine("Type the temperature in Celsius:");
+            decimal celcius = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processing 
+            decimal result = (celcius * 9 / 5) + 32;
+            #endregion
+
+            #region output
+            Console.WriteLine($"The temperature in Fahrenheit: {result}");
+            Console.ReadLine();
+            #endregion
         }
     }
 }
