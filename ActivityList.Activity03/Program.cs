ad956dfd7b708926ffe5e0eb67e439e905aa1091
josewerlich <werlich@outlook.com>
2025-04-03 18:54:19 -0400

    Activity 03 - Gas consumption

---
+++ ActivityList.Activity03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ActivityList.Activity03
+{
+    internal class Program
+    {
+        //create a program to calcule the gas consumption of a car
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("Type the initial odometer reading :");
+            decimal initialOdometer = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the final odometer reading :");
+            decimal finalOdometer = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the gas used in liters:");
+            decimal gasUsed = Convert.ToDecimal(Console.ReadLine());
+
+            #endregion
+
+            #region processing 
+            decimal result = (finalOdometer - initialOdometer) / gasUsed;
+            #endregion
+
+            #region output
+            Console.WriteLine($"The consumption was {result} liters");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
