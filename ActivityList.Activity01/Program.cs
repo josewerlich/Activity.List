07a1991f9a84c64af6f381a1bb065e9593daf2d9
josewerlich <werlich@outlook.com>
2025-04-03 18:32:22 -0400

    Add project files.

---
+++ ActivityList.Activity01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ActivityList.Activity01
+{
+    internal class Program
+    {
+
+        //Create a program to calculate the volume of a rectangular box
+
+        static void Main(string[] args)
+        {
+
+
+            #region input
+            Console.WriteLine("Type the lenght of the box:");
+            decimal length = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the width of the box:");
+            decimal width = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the height of the box:");
+            decimal height = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processing 
+            decimal result = length * width * height;
+            #endregion
+
+            #region output
+            Console.WriteLine($"The volume of the box is: {result}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
