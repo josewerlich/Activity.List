c8748cf2ab3de8788425fa6a47ca1eef96fd2e0a
josewerlich <werlich@outlook.com>
2025-04-03 19:59:35 -0400

    Activity 06 - Weighted calc

---
+++ ActivityList.Activity06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ActivityList.Activity06
+{
+    internal class Program
+    {
+        //create a program to calculate the  weighted mean for two test
+        static void Main(string[] args)
+        {
+            #region input
+            Console.WriteLine("Type the first test result: ");
+            decimal firstTest = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the first test weight:");
+            decimal firstTestWeight = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the second test result: ");
+            decimal secondTest = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Type the second test weight:");
+            decimal secondTestWeight = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processing 
+            decimal result = (firstTest * firstTestWeight+secondTest * secondTestWeight)/(firstTestWeight + secondTestWeight);
+            #endregion
+
+            #region output
+            Console.WriteLine($"The GPA is: {result}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
---
+++ ActivityList05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ActivityList05
+{
+    internal class Program
+    {
+        //create a program to calculate a salary with base + comission. 
+        //inform the base salary + total sales and the % of comission.
+        static void Main(string[] args)
+        {
+            #region input
+            Console.Write("Type base salary:$ ");
+            decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Type the total sales:$");
+            decimal sales = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Type the percentage of commission:$ ");
+            decimal comission = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processing 
+            decimal result = ((sales * comission)/100) + salary;
+            #endregion
+
+            #region output
+            Console.WriteLine($"The salary + commission is: ${result}");
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
