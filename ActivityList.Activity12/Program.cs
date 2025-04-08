b0548f428b0994f3ca654c5d0e6238f587694aec
josewerlich <werlich@outlook.com>
2025-04-08 18:17:17 -0400

    create a number to write odd numbers between 100 and 200

---
+++ ActivityList.Activity12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ActivityList.Activity12
+{
+    internal class Program
+    {
+        //create a number to write odd numbers between 100 and 200
+        static void Main(string[] args)
+        {
+            for (int counter = 101; counter <= 199; counter += 2)
+            {
+                int oddnumber = Convert.ToInt32(counter);
+                Console.WriteLine(oddnumber);
+            }
+            Console.ReadLine();
+        }
+    }
+}
