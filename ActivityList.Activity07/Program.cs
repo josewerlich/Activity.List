ebf46399202fa98c9bc57e7b4acb8b71ce8c77bf
josewerlich <werlich@outlook.com>
2025-04-08 16:58:44 -0400

    Create a program to identify if the number is a prime number

---
+++ ActivityList.Activity07/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ActivityList.Activity07
+{
+    internal class Program
+    {
+        //create a program to identify if the number is a prime number.
+        static void Main(string[] args)
+        {
+
+                #region input
+                Console.WriteLine("Type a number:");
+                int number = Convert.ToInt32(Console.ReadLine());
+                #endregion
+
+                #region processing 
+                if (number == 0 || number == 1)
+                {
+                    Console.WriteLine($"The number {number} is not prime.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    for (int a = 2; a <= number / 2; a++)
+                    {
+                      
+                        if (number % a == 0)
+                        {
+                        Console.WriteLine($"The number {number} is not a prime number");
+                        Console.ReadLine();
+                        return;
+                        }
+                    }
+
+                    Console.WriteLine($"The number {number} is a prime number");
+                    Console.ReadLine();
+                #endregion
+
+                }
+
+            
+
+
+        }
+    }
+}
