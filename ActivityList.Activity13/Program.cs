0fa0b30dd168c55746c23edfeeba89fb92081297
josewerlich <werlich@outlook.com>
2025-04-08 18:24:37 -0400

    create a program to read a number and print the factorial calculation

---
+++ ActivityList.Activity13/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ActivityList.Activity13
+{
+    internal class Program
+    {
+        //create a program to read a number and print the factorial calculation
+        static void Main(string[] args)
+        {
+            int number, factorial = 1;
+
+            Console.Write("Type a number: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(number + "! = ");
+
+            for (int counter = number; counter >= 1; counter--)
+
+            {
+
+                Console.Write(counter + " ");
+
+                factorial *=  counter;
+
+                if (counter > 1)
+
+                {
+
+                    Console.Write("X ");
+
+                }
+
+            }
+
+            Console.Write("= " + factorial);
+
+            Console.ReadLine();
+        }
+    }
+}
