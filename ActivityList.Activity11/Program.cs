3f6e5186d264690ac89ec6f4987f75fdc4ed1d78
josewerlich <werlich@outlook.com>
2025-04-08 17:47:38 -0400

    create a program to inform if a number is even or odd

---
+++ ActivityList.Activity11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ActivityList.Activity11
+{
+    internal class Program
+    {
+        //create a program to inform if a number is even or odd
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the number: ");
+            decimal N = Convert.ToDecimal(Console.ReadLine());
+
+            if (N % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
