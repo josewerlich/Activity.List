4b982dd93781f896635bc2691c5f11fcd8393178
josewerlich <werlich@outlook.com>
2025-04-08 18:55:25 -0400

    create a program to read a number and show the Fibonacci sequency

--- ActivityList.Activity14/Program.cs
+++ ActivityList.Activity14/Program.cs
@@ -2,37 +2,35 @@
 {
     internal class Program
     {
+
+        //create a program to read a number and show the Fibonacci sequency
         static void Main(string[] args)
         {
-            //create a program to show the Fibonacci calc of a number
-            static void Main(string[] args)
+            Console.Write("Type a number: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int fibonacci1 = 0, fibonacci2 = 1;
+
+            Console.Write(fibonacci1 + " " + fibonacci2 + " ");
+
+            while (fibonacci2 < number)
+
             {
-                Console.Write("Type a number: ");
 
-                int number = Convert.ToInt32(Console.ReadLine());
+                int fibonacci3 = fibonacci1 + fibonacci2;
 
-                int fibonacci1 = 0, fibonacci2 = 1;
+                Console.Write(fibonacci3 + " ");
 
-                Console.Write(fibonacci1 + " " + fibonacci2 + " ");
+                fibonacci1 = fibonacci2;
 
-                while (fibonacci2 < number)
+                fibonacci2 = fibonacci3;
 
-                {
-
-                    int fibonacci3 = fibonacci1 + fibonacci2;
-
-                    Console.Write(fibonacci3 + " ");
-
-                    fibonacci1 = fibonacci2;
-
-                    fibonacci2 = fibonacci3;
-
-                }
-
-                Console.WriteLine();
-
-                Console.ReadLine();
             }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
     }
 }
